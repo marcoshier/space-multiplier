5359b74ce1b3b258c8f8656fd1082ed763d36b77
marco <dellabatemarco@gmail.com>
2023-10-19 17:13:40 +0200

    projtester update

--- src/main/kotlin/ProjectionSurfaceTest.kt
+++ src/main/kotlin/ProjectionSurfaceTest.kt
@@ -2,39 +2,45 @@ import classes.Mapper
 import lib.UIManager
 import org.openrndr.application
 import org.openrndr.color.ColorRGBa
+import org.openrndr.draw.colorBuffer
+import org.openrndr.draw.createEquivalent
+import org.openrndr.drawImage
 import org.openrndr.extra.fx.Post
 import org.openrndr.extra.fx.patterns.Checkers
+import org.openrndr.extra.olive.oliveProgram
 import org.openrndr.extra.viewbox.viewBox
+import org.openrndr.math.IntVector2
 
 fun main() = application {
 
     configure {
         width = 1920
         height = 1000
+        //hideWindowDecorations = true
+        windowResizable = true
+        position = IntVector2(0, 0)
     }
 
     program {
-       val bg = viewBox(drawer.bounds) {
-            extend(Post()) {
-                val c = Checkers().apply {
-                    background = ColorRGBa.RED
-                    foreground = ColorRGBa.RED.shade(0.666)
-                    size = 1.0
-                }
-                post { input, output ->
-                    c.apply(input, output)
-                }
-            }
-        }
 
-        val uiManager = UIManager(program)
-        val mapper = Mapper() {
-            mapperElement("one", drawer.bounds.offsetEdges(-10.0).contour) { bg.result }
-        }
 
-        extend(mapper)
         extend {
-            bg.update()
+            val cb0 = colorBuffer(width, height)
+            val cb1 = cb0.createEquivalent()
+
+            val c = Checkers().apply {
+                background = ColorRGBa.RED
+                foreground = ColorRGBa.RED.shade(0.666)
+                size = 1.0
+            }
+
+            c.apply(cb0, cb1)
+            drawer.image(cb1)
+
+            drawer.text("width $width", 30.0, 30.0)
+            drawer.text("height $height", 30.0, 60.0)
+            drawer.text("position ${window.position}",30.0, 90.0)
+
         }
     }
 
